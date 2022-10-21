1881f4a56273133cba3f3748a88f483ed3c327e3
«evgeny» <solov-ghenya195@mail.ru>
2022-10-22 01:44:30 +0300

    add change task_03

--- task_03.py
+++ task_03.py
@@ -1,12 +1,9 @@
-
 def bin(n):
     y = []
     while n>0:
-        a = x%2
+        y.append(n%2)
         n = int(n/2)
-        y.append(a)
     return y
-
 x = int(input("Введите число: "))
 res = bin(x)
 res.reverse()
